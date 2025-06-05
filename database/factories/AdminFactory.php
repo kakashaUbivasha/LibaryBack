8a005b42c1eefe20cd52c22af3c258551b6aa425
kakashaUbivasha <makspala777@gmail.com>
2025-06-05 21:48:01 +0500

    seeder admin

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>'Admin',
+            'email'=>'admin@admin.com',
+            'password' => Hash::make('password'),
+            'role'=>'Admin'
+        ];
+    }
+}
