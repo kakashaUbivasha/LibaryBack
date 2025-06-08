d2ae1c66268768211fc8b5cc315f3870bffc02ef
kakashaUbivasha <makspala777@gmail.com>
2025-06-08 21:25:34 +0500

    ai recomendation update

--- app/Http/Controllers/AI/RecommendationController.php
+++ app/Http/Controllers/AI/RecommendationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AI;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use App\Services\GeminiService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -11,6 +12,7 @@ class RecommendationController extends Controller
 {
     public function index(GeminiService $gemini){
         $user = auth()->user();
+        $tags = Tag::all()->pluck('name')->toArray();
         $views_books = $user->views()->with('book')->orderBy('updated_at', 'desc')->take(10)->get();
         $reservations_books = $user->reservations()->with('book')->orderBy('updated_at', 'desc')->take(10)->get();
         if($views_books->count() < 10 && $reservations_books->count() < 5)
@@ -26,7 +28,7 @@ class RecommendationController extends Controller
         foreach ($views_books as $book) {
             $prompt .= "1. Название: \"{$book->book->title}\", Описание: \"{$book->book->description}\"\n";
         }
-        $prompt .= "\nНа основе этих книг определи предпочтения пользователя и предложи 5–7 новых книг, которые ему точно подойдут. Учитывай жанры, атмосферу и стиль произведений.
+        $prompt .= "\nНа основе этих книг определи предпочтения пользователя и предложи ему 3 тэга из списка тэгов: [\"{$tags['name']}\"], которые ему точно подойдут. Учитывай жанры, атмосферу и стиль произведений.
 
         Формат ответа:
         1. Название книги — краткое объяснение, почему она подойдёт.";
